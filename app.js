787954976c76dbefb0bd3b9b6a9889b1f10535cc
Iryna Artymuk <artimyk@ukr.net>
2023-09-01 12:39:55 +0300

    feat(add express temolate): add express template router
    n

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
+
+  
+
+const app = express();
+
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+
+app.use(logger(formatsLogger))
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
+export default app
+
