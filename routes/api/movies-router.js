e965c9d947e7c2be45891b5858b1604a71e60df7
Iryna Artymuk <artimyk@ukr.net>
2023-09-02 21:57:15 +0300

    feat(movie router): add delete method to router
    add delete method to router now posibl to del movie if you will pass the id to function movies
    service

--- routes/api/movies-router.js
+++ routes/api/movies-router.js
@@ -137,7 +137,29 @@ moviesRouter.put('/:id', async (req, res, next) => {
   }
 });
 
-moviesRouter.delete('/:id', async (req, res, next) => {});
+moviesRouter.delete('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const result = await moviesService.deleteMovieById(id);
+    // ЯКЩО ВІДПОВІДЬ З БЕКЕНДУ NULL СТВОРЮЄМ І ВИКИДАЄМ ПОМИЛКУ EXPRESS ПЕРХОДИТЬ В ФУНКЦІЮ ОБРОБКИ ПОМИЛОК
+    if (!result) {
+      throw HttpError(
+        404,
+        `обєкт з id:${id} не знайдено перевірте чи правильний id `
+      );
+    }
+    // ЯКЩО ТРЕБА ПОВЕРТУТИ СТАТУС 204 І   res.status(204).json(result) НЕ ПРАЦЮЄ БО STATUS 204 НЕ МАЄ BODY
+    // ТОМУ МОЖНА ПИСАТИ
+    // res.status(204).send()
+
+    res.json({
+      message: 'Delete success',
+    });
+    console.log('result: ', result);
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default moviesRouter;
 // імпортуєм в app.js і викорстовуєм в midlewear  app.use("/api/movies", moviesRouter);
