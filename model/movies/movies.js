12acd03f474820eae45b161e8d69e5e1ceaff0d5
Iryna Artymuk <artimyk@ukr.net>
2023-09-07 20:24:04 +0300

    fix(movie js): fix eror

--- model/movies/movies.js
+++ model/movies/movies.js
@@ -4,15 +4,14 @@
 
 import { Schema, model } from 'mongoose';
 // ------------create schema ------------
-const movieSchema = new Schems({
+const movieSchema = new Schema({
   title: String,
   director: String,
 });
 
+// створюєм екземпляр класу йому треба передати назву колекції в однині з якою буде працювати цей екземпляр і створену схему
+// експортуєм його для використання в контролерах
 
-// створюєм екземпляр класу йому треба передати назву колекції в однині з якою буде працювати цей екземпляр і створену схему 
-// експортуєм його для використання в контролерах 
+const Movie = model('movie', movieSchema);
 
-const Movie = model("movie",movieSchema )
-
-export default  Movie 
+export default Movie;
