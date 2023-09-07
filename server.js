8cce296900a8c88d5cf5e841759fd4e96e1b3f4a
Iryna Artymuk <artimyk@ukr.net>
2023-09-07 11:45:12 +0300

    feat(server js): add db host to  process.env

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 import app from './app.js';
 import mongoose from 'mongoose'; // імпортуєм монгус для підключення до бази даних
-const DB_HOST =
-  'mongodb+srv://Ira_Art:6PfXcX0yndb6EXh4@cluster0.mftd0fj.mongodb.net/my_movies?retryWrites=true&w=majority';
+// const DB_HOST =
+//   'mongodb+srv://Ira_Art:6PfXcX0yndb6EXh4@cluster0.mftd0fj.mongodb.net/my_movies?retryWrites=true&w=majority';
 
 // треба пердати строку підключення до бази  іі взяти треба в базі даних вкладка connect dreivers
 
 // ОБОВЯЗКОВО ТРЕБА ВКАЗАТИ НАЗВУ БАЗИ ДО ЯКОЇ ТРЕБА ПІДКЛЮЧИТИСЬ  В ЦЬОМУ ВИПАДКУ ЦЕ my_movies
 
 //mongoose.connect(DB_HOST) повертає проміс тому щоб не запускати сервер коли підключення до бази не успішне викорисовуємо then  catch
-
+const { DB_HOST, PORT = 3000 } = process.env;
+// під час деплою на сервері де буде деплоїтись проект бекенду треба в process.env (глобальний обєкт )сервера додати шлях підключення до бази
+// в Environment Variables треба додати ключ  DB_HOST і значення строка підключення
 mongoose
   .connect(DB_HOST)
   .then(() =>
