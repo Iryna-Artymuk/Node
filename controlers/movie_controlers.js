015a40d79fc893dd5d2359b4310f7e2cf6ab5895
Iryna Artymuk <artimyk@ukr.net>
2023-09-09 11:47:14 +0300

    feat(movie controler): mach joi and mongoose validation

--- controlers/movie_controlers.js
+++ controlers/movie_controlers.js
@@ -2,22 +2,40 @@ import Joi from 'joi'; // бібліотека валідації
 import Movie from '../model/movies/movies.js';
 
 import { HttpError } from '../helpers/index.js';
+
+//  ----Joi schema to check data from frontend
+// it is must match mongoose schema
 const movieAddSchema = Joi.object({
   title: Joi.string().required().messages({
     'any.required': `check again  if you  added   movie name `,
   }),
   director: Joi.string().required().messages({
-    'any.required': `check again  if you  added  directot `,
+    'any.required': `frontend validation error check again  if you  added  director`,
   }),
+  favourite: Joi.boolean(),
+  genre: Joi.string()
+    .valid('comedy', ' history', 'documentary')
+    .required()
+    .messages({
+      'any.only': ` frontend validation error should be a one  of  'comedy', ' history', 'documentary') `,
+    }),
+
+  releaseYear: Joi.string()
+    .regex(/^(19|20)\d{2}$/)
+    .required()
+    .messages({
+      'string.pattern.base': `frontend validation error  year must be  between 1900-2099`,
+      'any.required':
+        'Year should not be empty! check again  if you  added  year',
+    }),
 });
 
 const getAllMovies = async (req, res, next) => {
-  // Movie.find() це метод запиту до бази даних 
+  // Movie.find() це метод запиту до бази даних
   try {
     const result = await Movie.find();
 
     res.json(result);
-
   } catch (error) {
     next(error);
   }
@@ -25,15 +43,16 @@ const getAllMovies = async (req, res, next) => {
 const addMovie = async (req, res, next) => {
   console.log('req.body: ', req.body);
 
-  // дані які пердаються з фрогтенду придодять в req.body ми їх перевіряєм з доп joi
+  // дані які пердаються з фрогтенду придодять в req.body ми їх перевіряєм з доп joiSchema
   try {
-    // const validateResult = movieAddSchema.validate(req.body);
-    // const { error } = validateResult;
+    const validateResult = movieAddSchema.validate(req.body);
+    console.log('validateResult: ', validateResult);
+    const { error } = validateResult;
 
-    // if (error) throw HttpError(400, error.message);
+    if (error) throw HttpError(400, error.message);
     // якщо не буде всіх даних  error === true спрацює HttpError(400, validateResult.messages) і код перерветься спрацює функція обробки помилок
     //  error.message буде message з схеми валідації
-    const result = await Movie.create (req.body);
+    const result = await Movie.create(req.body);
 
     if (!result) {
       throw HttpError(
@@ -48,9 +67,9 @@ const addMovie = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-}
+};
 
 export default {
   getAllMovies,
-  addMovie 
+  addMovie,
 };
