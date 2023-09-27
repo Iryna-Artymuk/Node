ea6cf87e27023cf64e4bdbeebf6268a651f8526a
Iryna Artymuk <artimyk@ukr.net>
2023-09-27 16:42:19 +0300

    feat(app): add elastic email client

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import ElasticEmail from '@elasticemail/elasticemail-client';
 import express from 'express';
 import logger from 'morgan';
 import cors from 'cors';
@@ -29,5 +30,41 @@ app.use((err, req, res, next) => {
   const { status = 500, message = 'Server error' } = err;
   res.status(status).json({ message });
 });
+
+const { API_KEY, ELASTIC_EMAIL_FROM } = process.env;
+var defaultClient = ElasticEmail.ApiClient.instance;
+// Configure API key authorization: apikey
+const { apikey } = defaultClient.authentications;
+apikey.apiKey = API_KEY;
+// Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
+//apikey.apiKeyPrefix['X-ElasticEmail-ApiKey'] = "Token"
+
+const api = new ElasticEmail.EmailsApi();
+
+const email = ElasticEmail.EmailMessageData.constructFromObject({
+  Recipients: [
+    new ElasticEmail.EmailRecipient('cepsuherdi@gufum.com'),
+    new ElasticEmail.EmailRecipient('Artimyk@ukr.net'),
+  ],
+  Content: {
+    Body: [
+      ElasticEmail.BodyPart.constructFromObject({
+        ContentType: 'HTML',
+        Content: 'My test email content;)',
+      }),
+    ],
+    Subject: 'Test Email',
+    From: ELASTIC_EMAIL_FROM,
+  },
+});
+
+const callback = function (error, data, response) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('API called successfully.');
+  }
+};
+api.emailsPost(email, callback);
 
 export default app;
